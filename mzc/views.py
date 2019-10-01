a63ebf71aa007c13aa34adbd9f1dce502ea80364
aswathy19989 <aswathysasi876@gmail.com>
2019-10-01 16:25:09 +0530

    read

--- mzc/views.py
+++ mzc/views.py
@@ -14,5 +14,11 @@ def homepage(request):
     return render(request,'home.html')
 
 def facultypage(request):
+    if request.method == "POST":
+        form = FacultyForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('succesfully added')
+    else:
     form=FacultyForm()
     return render(request,'faculty.html',{'form':form})
